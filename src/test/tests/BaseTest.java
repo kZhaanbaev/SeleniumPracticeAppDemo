899089e15d02f8e42037c2dea6f2ddd8add8b219
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-06-01 12:01:44 -0400

    day 28

--- src/test/tests/BaseTest.java
+++ src/test/tests/BaseTest.java
@@ -3,6 +3,7 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -11,8 +12,7 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUpBase(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        initializeDriver("chrome");
         driver.get("https://selenium-practice-app.herokuapp.com/?#/home");
     }
 
@@ -20,4 +20,20 @@ public class BaseTest {
     public void tearDown(){
         driver.close();
     }
+
+    public void initializeDriver(String browserType){
+        switch (browserType.toLowerCase()){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            default:
+                System.out.println("Wrong browser type");
+        }
+    }
+
 }
--- src/test/tests/InputsPageTest.java
+++ src/test/tests/InputsPageTest.java
@@ -1,25 +1,15 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
+package tests;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class _01_ClassTask_Solved {
-    WebDriver driver;
+public class InputsPageTest extends BaseTest{
 
     @BeforeMethod
     public void setUp(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
         driver.get("https://selenium-practice-app.herokuapp.com/?#/inputs");
-    }
-
-    @AfterMethod
-    public void tearDown(){
-        driver.close();
     }
 
     @Test(testName = "US101: Verify Input message: test case 1", groups = {"regression"})
